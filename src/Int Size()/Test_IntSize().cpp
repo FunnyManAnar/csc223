17469ab474b3496738f39b20a9d407c7e8b5cec4
FunnyManAnar <hahappy_anar@yahoo.com>
2025-02-18 04:28:55 -0500

    Forgot parenthisis

--- src/Int Size()/Test_IntSize().cpp
+++ src/Int Size()/Test_IntSize().cpp
@@ -1,6 +1,6 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <iostream>
-#include "doctest/doctest.h"
+#include "doctest.h"
 #include "IntSize().h"
 #include <string>
 using namespace std;
@@ -8,13 +8,13 @@ using namespace std;
 
 TEST_CASE("Test if the John even works") {
     LinkedList<int> list;
-    CHECK(list.to_str() == "Empty List");
+    CHECK(list.to_string() == "Empty List");
     list.insert_at_front(1);
-    CHECK(list.to_str() == "(1)");
+    CHECK(list.to_string() == "(1)");
     list.insert_at_front(2);
-    CHECK(list.to_str() == "(2, 1)");
+    CHECK(list.to_string() == "(2, 1)");
     list.insert_at_front(3);
-    CHECK(list.to_str() == "(3, 2, 1)");
+    CHECK(list.to_string() == "(3, 2, 1)");
 }
 
 TEST_CASE("Test Size Grabber") {
@@ -22,8 +22,8 @@ TEST_CASE("Test Size Grabber") {
     list.insert_at_front(1);
     list.insert_at_front(2);
     list.insert_at_front(3);
-    CHECK(list.to_str() == "(3, 2, 1)");
-    CHECK(list.IntSize() == "(3)";
+    CHECK(list.to_string() == "(3, 2, 1)");
+    CHECK(list.IntSize() == "(3)");
 
 TEST_CASE("Test Size Grabber after a removal") {
     LinkedList<int> list;
@@ -32,6 +32,6 @@ TEST_CASE("Test Size Grabber after a removal") {
     list.insert_at_front(3);
     list.remove_from_front();
     CHECK(list.to_str() == "(2, 1)");
-    CHECK(list.IntSize() == "(2)";
+    CHECK(list.IntSize() == "(2)");
 
 }
