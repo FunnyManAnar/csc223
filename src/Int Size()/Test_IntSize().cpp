310127c3492b37bcec6a71a560d70cc79d2f1fcb
FunnyManAnar <hahappy_anar@yahoo.com>
2025-02-18 04:30:59 -0500

    Forgot more parenthesis

--- src/Int Size()/Test_IntSize().cpp
+++ src/Int Size()/Test_IntSize().cpp
@@ -24,6 +24,7 @@ TEST_CASE("Test Size Grabber") {
     list.insert_at_front(3);
     CHECK(list.to_string() == "(3, 2, 1)");
     CHECK(list.IntSize() == "(3)");
+}
 
 TEST_CASE("Test Size Grabber after a removal") {
     LinkedList<int> list;
@@ -31,7 +32,6 @@ TEST_CASE("Test Size Grabber after a removal") {
     list.insert_at_front(2);
     list.insert_at_front(3);
     list.remove_from_front();
-    CHECK(list.to_str() == "(2, 1)");
+    CHECK(list.to_string() == "(2, 1)");
     CHECK(list.IntSize() == "(2)");
-
 }
